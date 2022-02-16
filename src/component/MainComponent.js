17b24fd64334507d4f8529a2e8e65d4f29ceb130
Maruko Phuong <lcnphuong@gmail.com>
2022-02-16 16:10:42 +0900

    Install and configure Redux

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,52 +1,50 @@
-import { Navbar, NavbarBrand, InputGroup, InputGroupText, Input } from 'reactstrap';
 import React, { Component } from 'react';
 import Stafflist from "./Stafflist";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Staffdetail from './Staffdetail';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { STAFFS } from "../shared/staffs";
-import { DEPARTMENTS } from "../shared/staffs"
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Deparment from './Department';
 import Salary from './Salary';
+import { connect } from 'react-redux';
 
+const mapStateToProps = state => {
+    return {
+        staffs: state.staffs,
+        depts: state.depts,
+    }
+}
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            staffs: STAFFS, //create staffs = STAFFS import from staffs.jsx
-            depts: DEPARTMENTS //create depts = DEPARTMANET import from staffs.jsx
-        };
         this.addNewStaff = this.addNewStaff.bind(this);
-
     }
-
     addNewStaff(staff) {
-        var newStaffs = this.state.staffs;
+        var newStaffs = this.props.staffs;
         newStaffs.push(staff);
         this.setState({
             staffs: newStaffs
-        })        
+        })
     }
 
     render() {
         const StaffWithId = ({ match }) => {
             return (
                 //Show Staffdetail from staff.id filter
-                <Staffdetail staff={this.state.staffs.filter((staff) => staff.id === parseInt(match.params.filterID, 10))[0]} />);
+                <Staffdetail staff={this.props.staffs.filter((staff) => staff.id === parseInt(match.params.filterID, 10))[0]} />);
         };
         return (
             <div className="App">
                 <Header />
                 <Switch>
                     {/*Link to Stafflist*/}
-                    <Route exact path='/stafflist' component={() => <Stafflist staffs={this.state.staffs} addNewStaff={this.addNewStaff} />} />
+                    <Route exact path='/stafflist' component={() => <Stafflist staffs={this.props.staffs} addNewStaff={this.addNewStaff} />} />
                     {/*Link to Staffdetail when click Staffid*/}
                     <Route path='/stafflist/:filterID' component={StaffWithId} />
                     {/*Link to Deparment*/}
-                    <Route exact path='/dept' component={() => <Deparment depts={this.state.depts} />} />
+                    <Route exact path='/dept' component={() => <Deparment depts={this.props.depts} />} />
                     {/*Link to Salary*/}
-                    <Route exact path='/salary' component={() => <Salary staffs={this.state.staffs} />} />
+                    <Route exact path='/salary' component={() => <Salary staffs={this.props.staffs} />} />
                     {/*Default link*/}
                     <Redirect to='/stafflist' />
                 </Switch>
@@ -56,4 +54,4 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
---
+++ src/redux/reducer.js
@@ -0,0 +1,10 @@
+import { STAFFS } from "../shared/staffs";
+import { DEPARTMENTS } from "../shared/staffs"
+
+export const initialState = {
+    staffs: STAFFS, //create staffs = STAFFS import from staffs.jsx
+    depts: DEPARTMENTS //create depts = DEPARTMANET import from staffs.jsx
+}
+export const Reducer = (state = initialState, action) => {
+    return state;
+};
